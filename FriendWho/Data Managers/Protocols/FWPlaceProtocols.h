c58692652672dd4fb3e7f0e1448086a8c58820dd
kenmhaggerty <kenmhaggerty@gmail.com>
2016-03-04 11:59:34 -0500

    Creates FWPlaceProtocols.

--- FriendWho/Data Managers/Protocols/FWPlaceProtocols.h
+++ FriendWho/Data Managers/Protocols/FWPlaceProtocols.h
@@ -6,8 +6,42 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+#pragma mark - // NOTES //
+
+#pragma mark - // IMPORTS //
+
 #import <Foundation/Foundation.h>
 
-@protocol FWPlaceProtocols <NSObject>
+#pragma mark - // DEFINITIONS //
+
+#pragma mark - // PROTOCOL //
+
+@protocol FWPlace <NSObject>
+
+- (NSString *)name;
+- (NSString *)street;
+- (NSString *)city;
+- (NSString *)state;
+- (NSString *)zip;
+- (NSString *)country;
+- (CLLocationCoordinate2D)coordinates;
+
+@end
+
+#pragma mark - // PROTOCOL (FWPlace_PRIVATE) //
+
+@protocol FWPlace_PRIVATE <FWPlace>
+
+// INITIALIZERS //
+
+- (id)initWithName:(NSString *)name coordinates:(CLLocationCoordinate2D)coordinates;
+
+// SETTERS //
+
+- (void)setStreet:(NSString *)street;
+- (void)setCity:(NSString *)city;
+- (void)setState:(NSString *)state;
+- (void)setZip:(NSString *)zip;
+- (void)setCountry:(NSString *)country;
 
 @end
